05e2721b5c00abab2ebcad953ece3315751beeee
Mitsuhiko <n23014@std.it-college.ac.jp>
2024-05-07 12:58:22 +0000

    editted hello.py

--- CH01/hello.py
+++ CH01/hello.py
@@ -1,5 +1,5 @@
 name = input("あなたの名前を入力してください: ")
 print(f"こんにちは,{name}さん\n名前は{len(name)}文字です.")
 age = int(input("続いて,年齢を入力してください: ")) 
-print("未成年です." if age < 20 else "成人しています.")
+print(f"現在{age}歳で,来年には{age+1}歳になります.")
 
