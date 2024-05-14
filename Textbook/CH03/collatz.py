988c1c15629bc554b78b3f96ca00b1015789de93
Mitsuhiko <n23014@std.it-college.ac.jp>
2024-05-14 01:56:19 +0000

    collatz eddited

--- Textbook/CH03/collatz.py
+++ Textbook/CH03/collatz.py
@@ -2,9 +2,7 @@ def collatz(n):
     return n // 2 if n % 2 == 0 else 3 * n + 1
 
 number = int(input("数字を入力してください: "))
-r = collatz(number)
-print(r)
+print(r:= collatz(number))
 while r != 1:
-    r = collatz(r)
-    print(r)
+    print(r := collatz(r))
 
