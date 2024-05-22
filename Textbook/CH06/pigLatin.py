1160732d21eea56b325429aa14cac8b44920f490
Mitsuhiko <n23014@std.it-college.ac.jp>
2024-05-22 08:58:50 +0000

    piglatin A-Z added

--- Textbook/CH06/pigLatin.py
+++ Textbook/CH06/pigLatin.py
@@ -1,12 +1,12 @@
 def pig_latin(text):
     return ' '.join(
-        word + 'yay' if word[0] in 'aeiouAEIOU' else
-        word[2:] + word[:2] + 'ay' if word[:2].isalpha() and word[0] not in 'aeiouAEIOU' and word[1] not in 'aeiouAEIOU' else
-        word[1:] + word[0] + 'ay' if word[0].isalpha() else
+        word + ('YAY' if word[0].isupper() else 'yay') if word[0] in 'aeiouAEIOU' else
+        word[2:] + word[:2] + ('AY' if word[0].isupper() else 'ay') if word[:2].isalpha() and word[0] not in 'aeiouAEIOU' and word[1] not in 'aeiouAEIOU' else
+        word[1:] + word[0] + ('AY' if word[0].isupper() else 'ay') if word[0].isalpha() else
         word
         for word in text.split()
     )
 
-text = input("英語をピッグラテンに翻訳します:\n")
+text = input()
 print(pig_latin(text))
 
