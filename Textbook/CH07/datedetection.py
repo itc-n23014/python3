33c1ac46a0c0ddd6fdbca877dac5645acf5adf5e
Mitsuhiko <n23014@std.it-college.ac.jp>
2024-05-29 02:44:34 +0000

    editted file

--- Textbook/CH07/datedetection.py
+++ Textbook/CH07/datedetection.py
@@ -6,7 +6,7 @@ def date_detecter(s):
     d,m,y = map(int,[i for i  in regex.findall(s)][0])
     list[2] = 0 if not 999 < y < 3000 else 1
     list[1] = 1 if 0 < m < 13 else 0
-    list[0] = 1 if m in [1,3,5,7,8,10,12] and d < 32 | m in [3,4,6,9,11] and 0 < 31 | m == 2 and d < 29 else 0
+    list[0] = 1 if m in [1,3,5,7,8,10,12] and d < 32 or m in [3,4,6,9,11] and 0 < 31 or m == 2 and d < 29 else 0
     return all(list)
 
 
