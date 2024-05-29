4541be89f0a9480151f37ff364a24d6280c3cb5b
Mitsuhiko <n23014@std.it-college.ac.jp>
2024-05-29 01:23:28 +0000

    add multi.file

---
+++ Textbook/CH08/multiplicationQuiz.py
@@ -0,0 +1,21 @@
+import pyinputplus as p
+from random import randint
+import time
+
+questions = [f"{a}*{b}=" for a, b in [(randint(0, 9), randint(0, 9)) for _ in range(10)]]
+
+for q in questions:
+    ans = eval(q[:-1])
+    try:
+        for _ in range(3):
+            u_ans = p.inputInt(f'{q}', timeout=8, limit=3)
+            if ans == u_ans:
+                print("正解")
+                time.sleep(1)
+                break
+            else:
+                print('不正解')
+    except p.TimeoutException:
+        print("時間切れ!")
+    except p.RetryLimitException:
+        print("回数制限超え！")
