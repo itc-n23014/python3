6983fd85235ba84889865d7390b228bf1cd2490a
Mitsuhiko <n23014@std.it-college.ac.jp>
2024-06-04 14:47:54 +0000

    editted arranged

--- Textbook/CH09/sentence_generator.py
+++ Textbook/CH09/sentence_generator.py
@@ -1,37 +1,25 @@
 import sys
 import re
 
-if len(sys.argv) < 2:
-    sys.exit('使い方: sentence_generator.py src.txt')
+def filling(noun, adjective, verb):
+    with open("sec.txt", "r") as file:
+        template = file.read()
 
-src_file = open(sys.argv[1], 'r', encoding='utf-8') 
-src_data = src_file.read()
-src_file.close()
+    filled = re.sub(r'\[名詞\]', noun, template)
+    filled = re.sub(r'\[形容詞\]', adjective, filled)
+    filled = re.sub(r'\[動詞\]', verb, filled)
 
+    return filled
 
-patterns = {
-    '形容詞': 'かわいい',
-    '名詞': 'パンダ',
-    '動詞': '散歩している'
-}
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print("Usage: python3 script.py [名詞] [形容詞] [動詞]")
+        sys.exit(1)
 
-while True:
-    mo = re.search(r'\[([^]]+)\]', src_data)
-    if not mo:
-        break
+    noun = sys.argv[1]
+    adjective = sys.argv[2]
+    verb = sys.argv[3]
 
-    pattern_name = mo.group(1)
-    if pattern_name in patterns:
-        repl = patterns[pattern_name]
-    else:
-        print(f'Please provide a replacement for {pattern_name}:')
-        repl = input()
-
-    src_data = src_data.replace(mo.group(0), repl, 1)
-
-print(src_data, end='')
-
-dst_file = open(sys.argv[1] + '.generated.txt', 'w', encoding='utf-8') 
-dst_file.write(src_data)
-dst_file.close()
+    result = filling(noun, adjective, verb)
+    print(result)
 
