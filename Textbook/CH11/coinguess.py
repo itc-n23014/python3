f848f089372df2ce79b1b86e8214a9af2eca19a9
Mitsuhiko <n23014@std.it-college.ac.jp>
2024-06-11 01:51:45 +0000

    a

--- Textbook/CH11/coinguess.py
+++ Textbook/CH11/coinguess.py
@@ -1,10 +1,18 @@
 from random import randint
 
 def coin():
-    if (a := 0 if input('裏か表か: ') == '裏' else 1) == (b :=randint(0,1)):
-        print("当たり！！")
-    else:
-        print("ハズレ〜")
+    count = 0
+    while (count != 2):
+        if (a := 0 if input('裏か表か: ') == '裏' else 1) == (b :=randint(0,1)):
+            print("当たり！！")
+            break
+        else:
+            count += 1
+            print('ハズレ、もう一回頑張れ！'if count == 1 else '残念、苦手だね.') 
+
+
+
+
         
 
 coin()
