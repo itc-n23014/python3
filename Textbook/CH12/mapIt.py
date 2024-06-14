f619adecc77f64ab4244b82cb1933cd18a3ab805
Mitsuhiko <n23014@std.it-college.ac.jp>
2024-06-14 03:32:09 +0000

    a

--- Textbook/CH12/mapIt.py
+++ Textbook/CH12/mapIt.py
@@ -1,10 +1,14 @@
-import webbrowser,sys
+# 引数、またはクリップボードの場所をMapで表示するコード
+import webbrowser,sys,pyperclip
 arg = sys.argv
 
 if len(arg) < 2:
-    raise Exception("引数を指定してください.[python3 web.py 場所]")
-
-webbrowser.open(f'https://www.google.com/maps/place/{"+".join(arg[1:])}')
+    print('クリップボード上の場所を検索しています...')
+    p = pyperclip.paste().split()
+    webbrowser.open(f'https://www.google.com/maps/place/{"+".join(p)}')
+else:
+    print('GoogleMapから引数の場所を検索します...')
+    webbrowser.open(f'https://www.google.com/maps/place/{"+".join(arg[1:])}')
 
 
 
